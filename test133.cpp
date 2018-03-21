5eb65e62eea3fac6d16e659f6f4fcad724225b89
byt <1564047446@qq.com>
2018-03-21 11:33:47 +0800

    2018年 03月 21日 星期三 11:33:47 CST

--- test133.cpp
+++ test133.cpp
@@ -1,29 +1,32 @@
 #include <stdio.h>
 
-int a[10001];
-
-int is_prime(int x) {
-	for (int i = 2; i * i <= x; i++) {
-		if (x % i == 0) return 0;
-	}
-	return 1;
-}
-
-int dfs(int left, int temp_sum, int l, int r) {
-	if (left == 0) {
-		return is_prime(temp_sum);
+int main() {
+	int n;
+	scanf("%d", &n);
+	int cnt = 0, day = 1, flag = 1, up = 0, down = 0;
+	for (int i = 1; i <= n && flag; i++) {
+		if (i == 1) continue;
+		if (i % 2 == 0) {
+			++up;
+			for (int i = 1; i <= up; i++) {
+				if (day == n) {
+					flag = 0;
+					break;
+				}
+				day++;
+			}
+		} else {
+			++down;
+			day++;
+			if (day == n) {
+				flag = 0;
+				break;
+			}
+		}
 	}
 	int sum = 0;
-	for (int i = l; i <= r; i++) {
-		sum += dfs(left - 1, temp_sum + a[i], i + 1, r);
+	for (int i = 1; i <= up; i++) {
+		sum += i;
 	}
-	return sum;
-}
-
-int main() {
-	int n, k;
-	scanf("%d%d", &n, &k);
-	for (int i = 0; i < n; i++) scanf("%d", a + i);
-	int ans = dfs(k, 0, 0, n - 1);
-	printf("%d\n", ans);
+	printf("%d\n", sum - down);
 }
