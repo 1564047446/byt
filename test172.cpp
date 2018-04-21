118b77eeb94ee3f93bc52d5f2061c99a54754f0c
byt <1564047446@qq.com>
2018-04-21 15:32:16 +0800

    2018年 04月 21日 星期六 15:32:16 CST

--- test172.cpp
+++ test172.cpp
@@ -3,16 +3,20 @@
 int get_solution(int *arr, int n) {
 	int sum = 0, ans = 0;
 	for (int i = 0; i < 32; i++) {
+		sum = 0;
 		for (int j = 0; j < n; j++) {
-			sum += ((arr[j] >> 1) & 1);
+			sum += ((arr[j] >> i) & 1);
+
 		}
+		printf("%d\n", sum);
 		if (sum % 3 == 1) {
-			ans = 1 << i;
+			ans = ans | (1 << i);
 		}
 	}
 	printf("%d\n", ans);
 	return 0;
 }
+
 
 int main() {
 	int arr[1000], n;
