734ff9705d1f97566c100e171c284101519f77cc
byt <1564047446@qq.com>
2018-05-09 15:41:03 +0800

    2018年 05月 09日 星期三 15:41:03 CST

---
+++ a2.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: a2.cpp
+	> Author:  byt 
+	> Mail: baotongyu1995@163.com
+	> Created Time: 2018年05月09日 星期三 10时08分08秒
+ ************************************************************************/
+
+#include <iostream>
+#include <string>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    cout << a << endl;
+}
