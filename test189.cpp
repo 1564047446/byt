528fd9bd291561d30ace318a9e4a4e7a04169a64
byt <1564047446@qq.com>
2018-05-17 16:53:54 +0800

    2018年 05月 17日 星期四 16:53:53 CST

---
+++ test189.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: test189.cpp
+	> Author:  byt 
+	> Mail: baotongyu1995@163.com
+	> Created Time: 2018年05月15日 星期二 15时24分21秒
+ ************************************************************************/
+
+#include <iostream>
+#include <string>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+    int *p;
+    p = new int(2);
+    cout << *p << endl;
+}
