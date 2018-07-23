7a1e64fb211a3c5ac312b064207a494833b7617f
byt <1564047446@qq.com>
2018-07-23 22:12:39 +0800

    2018年 07月 23日 星期一 22:12:39 CST

---
+++ new_a.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: new_a.cpp
+	> Author: byt
+	> Mail: baotongyu@zt.game
+	> Created Time: 2018年07月22日 星期日 16时12分42秒
+ ************************************************************************/
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;;
+using std::string;
+using std::vector;
+
